0eaef751b94d3fd10fef9064e89dbe6a33159207
sebastian gonzalez trujillo <sebastiangonza1505@hotmail.com>
2023-02-04 15:45:32 -0500

    actu

--- src/Componentes/InicioSesion.jsx
+++ src/Componentes/InicioSesion.jsx
@@ -1,10 +1,8 @@
-import React  from "react";
-import {
-  BrowserRouter as Router,
-  Link,
-  useNavigate,
-} from "react-router-dom";
+
 import axios from "axios";
+import React, {useState}from "react";
+import { Link } from "react-router-dom";
+
 
 function InicioSesion() {
   //Hooks
